914153a8200a298a9d8880c5333bd6f9b9f3e6d1
orhel <orhel7@gmail.com>
2015-02-14 21:49:33 -0600

    Update Rectangulo.java

--- src/Rectangulo.java
+++ src/Rectangulo.java
@@ -1,40 +1,40 @@
 public class Rectangulo {
-
+	//Declaración de variables
 	int base, altura;
-	
+	//Contructor a utilizar
 	Rectangulo(int base, int altura)
 	{
 		this.base = base;
 		this.altura = altura;
 	}
-	
+	//aqui obtenemos el valor de la base
 	int getBase()
 	{
 		return base;
 	}
-	
+	//establecemos el valor de la base
 	void setBase(int base)
 	{
 		this.base = base;
 	}
-	
+	//obtenemos el valor de la altura
 	int getAltura()
 	{
 		return altura;
 	}
-	
+	//establecemos el valor de la altura
 	void setAltura(int altura)
 	{
 		this.altura = altura;
 	}
-	
+	//obtenemos el valor del area
 	int getArea()
 	{
-		return base*altura;
+		return base*altura;//formula para calcular el area
 	}
-	
+	//obtenemos el valor del perimetro
 	int getPerimetro()
 	{
-		return (base*2)+(altura*2);
+		return (base*2)+(altura*2);//formula para obtener el perimetro
 	}
 }
