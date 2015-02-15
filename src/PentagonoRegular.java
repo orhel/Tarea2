95f160cc2cf64a291e8f57257e4bc35adeea9d44
orhel <orhel7@gmail.com>
2015-02-14 21:41:48 -0600

    Update PentagonoRegular.java

--- src/PentagonoRegular.java
+++ src/PentagonoRegular.java
@@ -1,24 +1,24 @@
 public class PentagonoRegular {
-
+	//Declaración de variable
 	int lado;
-	
+	//creación de la clase
 	PentagonoRegular(int lado)
 	{
 		this.lado = lado;
 	}
-	
+	//aqui obtenemos el valor a la variable lado
 	int getLado()
 	{
 		return lado;
 	}
-	
+	//aqui establecemos el valor de la variable lado
 	void setLado(int lado)
 	{
 		this.lado = lado;
 	}
-	
+	//aqui obtenemos el valor de perimetro
 	int getPerimetro()
 	{
-		return lado*5;
+		return lado*5;//formula para el perimetro
 	}
 }
